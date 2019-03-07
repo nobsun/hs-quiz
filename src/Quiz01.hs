3391aed79ef65c3192cced9cc27bac3a504d3a00
Nobuo Yamashita <nobsun@sampou.org>
2019-03-07 11:15:06 +0900

    modify  types of  和 and 積

--- src/Quiz01.hs
+++ src/Quiz01.hs
@@ -10,56 +10,39 @@ Ok, one module loaded.
 -}
 module Quiz01 where
 
-newtype N a = N { unN :: (a -> a -> a) -> a }
+newtype N a = N {unN :: [a]}
+
+instance Show a => Show (N a) where
+  show (N xs) = if singleton xs then show $ head xs else show xs
+    where
+      singleton [_] = True
+      singleton _   = False
 
 instance Num a => Num (N a) where
-  (+) = mknbo (+)
-  (*) = mknbo (*)
-  (-) = mknbo (-)
-  abs = mknuo abs
-  signum = mknuo signum
-  fromInteger = N . const . fromInteger
-
-mknbo :: (a -> a -> a) -> (N a -> N a -> N a)
-mknbo f m n = N $ \ _ -> unN m f `f` unN n f
-
-mknuo :: (a -> a) -> (N a -> N a)
-mknuo f n = N $ \ g -> f (unN n g)
-  
-と :: N a -> N a -> N a
-と f g = N $ \ h -> h (unN f h) (unN g h)
-
-の :: N a -> (a -> a -> a) -> a
-の = unN
-
-infixr 0 `の`
-
-和 :: Num a => a -> a -> a
-和 = (+)
-
-積 :: Num a => a -> a -> a
-積 = (*)
-
-newtype M a = M { unM :: [a] }
-
-instance Num a => Num (M a) where
   (+) = mkmbo (+)
   (*) = mkmbo (*)
   (-) = mkmbo (-)
   abs = mkmuo abs
   signum = mkmuo signum
-  fromInteger x = M [fromInteger x]
+  fromInteger x = N [fromInteger x]
   
-mkmbo :: (a -> a -> a) -> (M a -> M a -> M a)
-mkmbo f m n = M [foldr1 f (unM m) `f` foldr1 f (unM n)]
+mkmbo :: (a -> a -> a) -> (N a -> N a -> N a)
+mkmbo f m n = N [foldr1 f (unN m) `f` foldr1 f (unN n)]
 
-mkmuo :: (a -> a) -> (M a -> M a)
-mkmuo f = M . (f <$>) . unM
+mkmuo :: (a -> a) -> (N a -> N a)
+mkmuo f = N . (f <$>) . unN
 
-ト :: M a -> M a -> M a
-ト x y = M $ unM x <> unM y
+と :: N a -> N a -> N a
+と m n = N $ unN m <> unN n
 
-ノ :: M a -> (a -> a -> a) -> a
-ノ = flip foldr1 . unM
+の :: N a -> (N a -> N a) -> N a
+の = flip ($)
 
-infixr 0 `ノ`
+aggr :: (a -> a -> a) -> (N a -> N a)
+aggr f n = N [foldl1 f (unN n)]
+
+和 :: Num a => N a -> N a
+和 = aggr (+)
+
+積 :: Num a => N a -> N a
+積 = aggr (*)
